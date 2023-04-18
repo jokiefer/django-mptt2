86753819228322d020a3ae63c79b18560cebc9ee
jokiefer <jonas.kiefer@live.com>
2023-04-18 18:35:23 +0200

    Update managers.py and models.py

--- mptt2/managers.py
+++ mptt2/managers.py
@@ -17,8 +17,7 @@ class TreeManager(Manager):
         node: Node,
         target: Node,
         position: str = "last-child",
-        allow_existing_pk: bool = False,
-        refresh_target: bool = True,):
+        allow_existing_pk: bool = False):
 
         if node.pk and not allow_existing_pk and self.filter(pk=node.pk).exists():
             raise ValueError(_("Cannot insert a node which has already been saved."))
@@ -34,9 +33,10 @@ class TreeManager(Manager):
             node.mptt_lft = target.rgt
             node.mptt_rgt = target.rgt + 1
             node.mptt_depth = target.mptt_depth + 1
-
             with atomic():
-                self.filter(mptt_rgt_gte=target.rgt).update(mptt_rgt=F("mptt_rgt") + 2)
-                self.filter(mptt_lft_lt=target.rgt).update(mptt_lft=F("mptt_lft") + 2 )
+                self.filter(mptt_tree=F("mptt_tree"), mptt_rgt_gte=target.rgt).update(mptt_rgt=F("mptt_rgt") + 2)
+                self.filter(mptt_tree=F("mptt_tree"), mptt_lft_lt=target.rgt).update(mptt_lft=F("mptt_lft") + 2 )
                 node.save()
         return node
+    
+    
--- mptt2/models.py
+++ mptt2/models.py
@@ -8,7 +8,7 @@ from django.db.models.constraints import CheckConstraint, UniqueConstraint
 from django.db.models.query import Q
 from django.db.models.expressions import F
 from django.utils.translation import gettext as _
-
+from django.db.transaction import atomic
 
 from mptt2.query import DescendantsQuery, AncestorsQuery, FamilyQuery, SiblingsQuery, TreeQuerySet
 
@@ -53,32 +53,88 @@ class Node(Model):
             )
         ]
 
+    @atomic
+    def delete(self, *args, **kwargs):
+        if self.has_leafs:
+            self.objects.filter(
+                    mptt_tree=self.mptt_tree, 
+                    mptt_rgt_gt=self.mptt_rgt
+                ).update(
+                    mptt_rgt=F("mptt_rgt") - self.child_count
+                )
+            self.objects.filter(
+                    mptt_tree=self.mptt_tree,
+                    mptt_lft_gt=self.mptt_rgt
+            ).update(
+                mptt_lft = F("mptt_lft") - self.child_count
+            )
+        else:
+            self.objects.filter(
+                    mptt_tree=self.mptt_tree,
+                    mptt_lft_gte=self.mptt_lft,
+                    mptt_rgt_lte=self.mptt_rgt
+            ).update(
+                mptt_rgt=F("mptt_rgt") - 1,
+                mptt_lft=F("mptt_lft") - 1,
+                parent=self.mptt_parent
+            )
+            self.objects.filter(
+                    mptt_rgt_gt=self.mptt_rgt
+            ).update(
+                mptt_rgt=F("mptt_rgt") - 2
+            )
+            self.objects.filter(
+                    mptt_lft_gt=self.mptt_rgt
+            ).update(
+                mptt_lft=F("mptt_lft") - 2
+            )
+        
+        return super().delete(*args, **kwargs)
     
+    def move_to(self):
+        # TODO:
+        pass
+
     def get_descendants(self, include_self=False, asc=False)-> QuerySet:
+        # FIXME: not based on the current node
         descendants = self.objects.filter(DescendantsQuery(include_self=include_self))
         return descendants.order_by("-mptt_lft") if asc else descendants
 
     def get_ancestors(self, include_self=False, asc=False) -> QuerySet:
+        # FIXME: not based on the current node
         ancestors = self.objects.filter(AncestorsQuery(include_self=include_self))
         return ancestors.order_by("-mptt_lft") if asc else ancestors
 
     def get_family(self, include_self=False, asc=False) -> QuerySet:
+        # FIXME: not based on the current node
         family = self.objects.filter(FamilyQuery(include_self=include_self))
         return family.order_by("-mptt_lft") if asc else family
     
     def get_siblings(self, include_self=False, asc=False) -> QuerySet:
+        # FIXME: not based on the current node
         siblings = self.objects.filter(SiblingsQuery(include_self=include_self))
         return siblings.order_by("-mptt_lft") if asc else siblings
     
+    @property
     def is_root_node(self) -> bool:
         return self.parent is None
     
-    def is_leaf_node(self):
+    @property
+    def is_leaf_node(self) -> bool:
         return (self.mptt_rgt - self.mptt_lft) // 2 == 0
     
-    def get_descendant_count(self):
+    @property
+    def has_leafs(self) -> bool:
+        return self.mptt_rgt - self.mptt_lft > 0
+    
+    @property
+    def descendant_count(self):
         if self.mptt_rgt is None:
             # node not saved yet
             return 0
         else:
             return (self.mptt_rgt - self.mptt_lft - 1) // 2
+        
+    @property
+    def child_count(self):
+        return self.mptt_rgt - self.mptt_lft + 1
