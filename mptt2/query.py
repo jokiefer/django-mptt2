9d6799aaf7aab29ef1184bd0c51dde8caa79ca1c
jokiefer <jonas.kiefer@live.com>
2023-04-18 10:46:34 +0200

    implement tests for all basic queries

--- mptt2/query.py
+++ mptt2/query.py
@@ -1,24 +1,39 @@
 from typing import Any, Dict
 from django.db.models.query_utils import Q
-from django.db.models.expressions import F, OuterRef
+from django.db.models.expressions import F, OuterRef, CombinedExpression
+from django.db.models.query import QuerySet
+
 
 class ConvertableQuery(Q):
     
+    def convert_field_ref_expression(self, children, key, value):
+        if isinstance(value, CombinedExpression):
+            # TODO: convert F to OuterRef objects
+            if isinstance(value.lhs, F):
+                value.lhs = OuterRef(value.lhs.name)
+            elif isinstance (value.rhs, F):
+                value.rhs = OuterRef(value.rhs.name)
+        else:
+            index = children.index((key, value))
+            children[index] = (key, OuterRef(value.name))
+
     def to_subquery(self):
-        for key, value in self.children:
-            if "mptt_" in key:
-                # TODO: convert F to OuterRef objects
-                if isinstance(value.lhs, F):
-                    pass
-                elif isinstance (value.rhs, F):
-                    pass
+        """To use this query inside a subquery, this function converts the current query with F object references to OuterRef references."""
+        for child in self.children:
+            if isinstance(child, tuple):
+                key, value = child
+                self.convert_field_ref_expression(children=self.children, key=key, value=value)
+            else:
+                # we've got a list of Q nodes; so we need to call the to_subquery() of the Custom Q node to convert them.
+                if hasattr(child, "to_subquery"):
+                    child.to_subquery()
 
 class ParentQuery(ConvertableQuery):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(
             mptt_lft=F("mptt_lft") - 1,
             mptt_rgt=F("mptt_rgt") + 1,
-            tree_id=F("tree_id"),
+            mptt_tree_id=F("mptt_tree_id"),
             *args, 
             **kwargs
         )
@@ -51,35 +66,29 @@ class AncestorsQuery(ConvertableQuery):
         )
 
 
+
+
 class FamilyQuery(DescendantsQuery):
     def __init__(self, include_self: bool = False, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        self.__or__(
-            other=AncestorsQuery(include_self=include_self)
-        )
+        super().__init__(include_self=include_self, *args, **kwargs)
+        self.add(data=AncestorsQuery(include_self=include_self), conn_type=self.OR)
+        
 
 
 class ChildrenQuery(DescendantsQuery):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(mptt_depth=self.sub_expression_type("mptt_depth") + 1, *args, **kwargs)
+        super().__init__(mptt_depth=F("mptt_depth") + 1, *args, **kwargs)
 
 
 class LeafNodesQuery(DescendantsQuery):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(mptt_lft=self.sub_expression_type("mptt_rgt") - 1, *args, **kwargs)
+        super().__init__(mptt_lft=F("mptt_rgt") - 1, *args, **kwargs)
 
 
-class DescendantCountQuery(ConvertableQuery): 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(
-            (F("mptt_rgt") - F("mptt_lft")) // 2,
-            *args, 
-            **kwargs
-        )
 
-
-# TODO:
-# class TreeQuerySet(QuerySet):
+class TreeQuerySet(QuerySet):
     
-#     def get_parent(self):
-#         self.filter()
+    def with_descendant_count(self):
+        self.annotate(descendant_count=F("mptt_rgt") - F("mptt_lft") // 2)
+
+    
--- tests/tests_query.py
+++ tests/tests_query.py
@@ -1,10 +1,10 @@
 from django.test import SimpleTestCase
 
-from mptt2.query import ParentQuery
+from mptt2.query import ParentQuery, DescendantsQuery, AncestorsQuery, FamilyQuery, ChildrenQuery, LeafNodesQuery, DescendantCountQuery
 
 
 from django.db.models.query_utils import Q
-from django.db.models.expressions import F, OuterRef, Subquery
+from django.db.models.expressions import F, OuterRef, Value
 
 class QTestMixin(object):
     
@@ -21,12 +21,12 @@ class QTestMixin(object):
 class TestParentQuery(QTestMixin, SimpleTestCase):
     
         
-    def test_parent_query(self):
+    def test_default_query(self):
         query = ParentQuery()
         expected = Q(
             mptt_lft=F("mptt_lft") - 1,
             mptt_rgt=F("mptt_rgt") + 1,
-            tree_id=F("tree_id"),)
+            mptt_tree_id=F("mptt_tree_id"))
         self.assertQEqual(expected, query)
 
 
@@ -36,5 +36,228 @@ class TestParentQuery(QTestMixin, SimpleTestCase):
         expected = Q(
             mptt_lft=OuterRef("mptt_lft") - 1,
             mptt_rgt=OuterRef("mptt_rgt") + 1,
-            tree_id=OuterRef("tree_id"),)
+            mptt_tree_id=OuterRef("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    
+class TestDescendantsQuery(QTestMixin, SimpleTestCase):
+    
+        
+    def test_default_query(self):
+        query = DescendantsQuery()
+        expected = Q(
+            mptt_lft_lt=F("mptt_lft"),
+            mptt_rgt_gt=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_default_query_include_self(self):
+        query = DescendantsQuery(include_self=True)
+        expected = Q(
+            mptt_lft_lte=F("mptt_lft"),
+            mptt_rgt_gte=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_subquery(self):
+        query = DescendantsQuery()
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lt=OuterRef("mptt_lft"),
+            mptt_rgt_gt=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_subquery_include_self(self):
+        query = DescendantsQuery(include_self=True)
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lte=OuterRef("mptt_lft"),
+            mptt_rgt_gte=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+
+class TestAncestorsQuery(QTestMixin, SimpleTestCase):
+    
+        
+    def test_default_query(self):
+        query = AncestorsQuery()
+        expected = Q(
+            mptt_lft_gt=F("mptt_lft"),
+            mptt_rgt_lt=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_default_query_include_self(self):
+        query = AncestorsQuery(include_self=True)
+        expected = Q(
+            mptt_lft_gte=F("mptt_lft"),
+            mptt_rgt_lte=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_subquery(self):
+        query = AncestorsQuery()
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_gt=OuterRef("mptt_lft"),
+            mptt_rgt_lt=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_subquery_include_self(self):
+        query = AncestorsQuery(include_self=True)
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_gte=OuterRef("mptt_lft"),
+            mptt_rgt_lte=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+
+class TestFamilyQuery(QTestMixin, SimpleTestCase):
+    
+    def test_default_query(self):
+        query = FamilyQuery()
+        expected = Q(
+            mptt_lft_lt=F("mptt_lft"),
+            mptt_rgt_gt=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id")) | Q(
+            mptt_lft_gt=F("mptt_lft"),
+            mptt_rgt_lt=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_default_query_include_self(self):
+        query = FamilyQuery(include_self=True)
+        expected = Q(
+            mptt_lft_lte=F("mptt_lft"),
+            mptt_rgt_gte=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id")) | Q(
+            mptt_lft_gte=F("mptt_lft"),
+            mptt_rgt_lte=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_subquery(self):
+        query = FamilyQuery()
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lt=OuterRef("mptt_lft"),
+            mptt_rgt_gt=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id")) | Q(
+            mptt_lft_gt=OuterRef("mptt_lft"),
+            mptt_rgt_lt=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+    def test_subquery_include_self(self):
+        query = FamilyQuery(include_self=True)
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lte=OuterRef("mptt_lft"),
+            mptt_rgt_gte=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id")) | Q(
+            mptt_lft_gte=OuterRef("mptt_lft"),
+            mptt_rgt_lte=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"))
+        self.assertQEqual(expected, query)
+
+
+
+class TestChildrenQuery(QTestMixin, SimpleTestCase):
+    
+    def test_default_query(self):
+        query = ChildrenQuery()
+        expected = Q(
+            mptt_lft_lt=F("mptt_lft"),
+            mptt_rgt_gt=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"),
+            mptt_depth=F("mptt_depth") +1)
+        self.assertQEqual(expected, query)
+
+    def test_default_query_include_self(self):
+        query = ChildrenQuery(include_self=True)
+        expected = Q(
+            mptt_lft_lte=F("mptt_lft"),
+            mptt_rgt_gte=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"),
+            mptt_depth=F("mptt_depth") +1)
+        self.assertQEqual(expected, query)
+
+    def test_subquery(self):
+        query = ChildrenQuery()
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lt=OuterRef("mptt_lft"),
+            mptt_rgt_gt=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"),
+            mptt_depth=OuterRef("mptt_depth") +1)
+        self.assertQEqual(expected, query)
+
+    def test_subquery_include_self(self):
+        query = ChildrenQuery(include_self=True)
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lte=OuterRef("mptt_lft"),
+            mptt_rgt_gte=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"),
+            mptt_depth=OuterRef("mptt_depth") +1)
+        self.assertQEqual(expected, query)
+
+
+class TestLeafNodesQuery(QTestMixin, SimpleTestCase):
+    
+    def test_default_query(self):
+        query = LeafNodesQuery()
+        expected = Q(
+            mptt_lft_lt=F("mptt_lft"),
+            mptt_rgt_gt=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"),
+            mptt_lft=F("mptt_rgt") - 1)
+        self.assertQEqual(expected, query)
+
+    def test_default_query_include_self(self):
+        query = LeafNodesQuery(include_self=True)
+        expected = Q(
+            mptt_lft_lte=F("mptt_lft"),
+            mptt_rgt_gte=F("mptt_rgt"),
+            mptt_tree_id=F("mptt_tree_id"),
+            mptt_lft=F("mptt_rgt") - 1)
+        self.assertQEqual(expected, query)
+
+    def test_subquery(self):
+        query = LeafNodesQuery()
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lt=OuterRef("mptt_lft"),
+            mptt_rgt_gt=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"),
+            mptt_lft=OuterRef("mptt_rgt") - 1)
+        self.assertQEqual(expected, query)
+
+    def test_subquery_include_self(self):
+        query = LeafNodesQuery(include_self=True)
+        query.to_subquery()
+        expected = Q(
+            mptt_lft_lte=OuterRef("mptt_lft"),
+            mptt_rgt_gte=OuterRef("mptt_rgt"),
+            mptt_tree_id=OuterRef("mptt_tree_id"),
+            mptt_lft=OuterRef("mptt_rgt") - 1)
+        self.assertQEqual(expected, query)
+
+
+class TestDescendantsCountQuery(QTestMixin, SimpleTestCase):
+    
+    def test_default_query(self):
+        query = DescendantCountQuery()
+        expected = Q(
+            (F("mptt_rgt") - F("mptt_lft")))
+        self.assertQEqual(expected, query)
+
+    def test_subquery(self):
+        query = DescendantCountQuery()
+        query.to_subquery()
+        expected = Q((OuterRef("mptt_rgt") - OuterRef("mptt_lft")) )
         self.assertQEqual(expected, query)
