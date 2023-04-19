3162fd5c2dce7193aa8ef8112d35905196abe8e1
jokiefer <jonas.kiefer@live.com>
2023-04-19 12:23:29 +0200

    adds a tree to the fixturre file

--- tests/tests_models.py
+++ tests/tests_models.py
@@ -6,18 +6,36 @@ class TestNodeModel(TestCase):
     fixtures = ["simple_nodes.json"]
 
     def test_delete(self):
-        node: SimpleNode = SimpleNode.objects.get(pk=4)
+        node: SimpleNode = SimpleNode.objects.get(pk=18)
         node.delete()
 
-        recalculated_tree = SimpleNode.objects.filter(mptt_tree__pk=1)
+        recalculated_tree = SimpleNode.objects.filter(mptt_tree__pk=2)
 
-        self.assertEqual(recalculated_tree.count(), 3)
+        self.assertEqual(recalculated_tree.count(), 9)
         
         self.assertEqual(recalculated_tree[0].mptt_lft, 1)
-        self.assertEqual(recalculated_tree[0].mptt_rgt, 6)
+        self.assertEqual(recalculated_tree[0].mptt_rgt, 18)
 
         self.assertEqual(recalculated_tree[1].mptt_lft, 2)
-        self.assertEqual(recalculated_tree[1].mptt_rgt, 3)
+        self.assertEqual(recalculated_tree[1].mptt_rgt, 5)
 
-        self.assertEqual(recalculated_tree[2].mptt_lft, 4)
-        self.assertEqual(recalculated_tree[2].mptt_rgt, 5)
+        self.assertEqual(recalculated_tree[2].mptt_lft, 3)
+        self.assertEqual(recalculated_tree[2].mptt_rgt, 4)
+
+        self.assertEqual(recalculated_tree[3].mptt_lft, 6)
+        self.assertEqual(recalculated_tree[3].mptt_rgt, 11)
+
+        self.assertEqual(recalculated_tree[4].mptt_lft, 7)
+        self.assertEqual(recalculated_tree[4].mptt_rgt, 8)
+
+        self.assertEqual(recalculated_tree[5].mptt_lft, 9)
+        self.assertEqual(recalculated_tree[5].mptt_rgt, 10)
+
+        self.assertEqual(recalculated_tree[6].mptt_lft, 12)
+        self.assertEqual(recalculated_tree[6].mptt_rgt, 17)
+
+        self.assertEqual(recalculated_tree[7].mptt_lft, 13)
+        self.assertEqual(recalculated_tree[7].mptt_rgt, 16)
+
+        self.assertEqual(recalculated_tree[8].mptt_lft, 14)
+        self.assertEqual(recalculated_tree[8].mptt_rgt, 15)
