2bdecc31b5b900a918160939ff52272eb2d707c7
joki <jonas.kiefer@live.com>
2023-04-28 13:54:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
