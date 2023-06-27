d3ee70f55111476987efd2630229d90a282f2b2f
jokiefer <jonas.kiefer@live.com>
2023-06-27 10:43:30 +0200

    Update quality_assurance.yml and tests_admin_e2e.py

--- tests/tests_admin_e2e.py
+++ tests/tests_admin_e2e.py
@@ -14,12 +14,10 @@ class TestDragAndDrop(StaticLiveServerTestCase):
         os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
         super().setUpClass()
         cls.playwright = sync_playwright().start()
-        cls.browser = cls.playwright.chromium.launch()
 
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
-        cls.browser.close()
         cls.playwright.stop()
 
     def setUp(self):
@@ -27,18 +25,27 @@ class TestDragAndDrop(StaticLiveServerTestCase):
         client = Client()
         client.login(username="superuser", password='12345678') #Native django test client
         morsel = client.cookies['sessionid']
-
-        cookie_dict = {
+        self.cookie_dict = {
             "name": morsel.key,
             "value": morsel.value,
             "url": self.live_server_url,
         }
         
-        self.context = self.browser.new_context()
-        self.context.add_cookies([cookie_dict])
-        
+    def tearDown(self):
+        super().tearDown()
+        self.browser.close()
 
-    def test_drag_node_five_to_node_three(self):
+    def test_chromium(self):
+        self.browser = self.playwright.chromium.launch()
+        self._test_drag_node_five_to_node_three()
+
+    def test_firefox(self):
+        self.browser = self.playwright.firefox.launch()
+        self._test_drag_node_five_to_node_three()
+
+    def _test_drag_node_five_to_node_three(self):
+        self.context = self.browser.new_context()
+        self.context.add_cookies([self.cookie_dict])
         # just a simple e2e test to figure out static sortablejs lib is provided as static file and drag and drop is principle working
         page = self.context.new_page()
         page.goto(f"{self.live_server_url}/admin/tests/simplenode/")
@@ -49,6 +56,6 @@ class TestDragAndDrop(StaticLiveServerTestCase):
 
         node_three = page.locator('li[data-target-id="2"]')
 
-        node_five.drag_to(target=node_three, target_position={"x": 0, "y": 1}, )
+        node_five.drag_to(target=node_three, target_position={"x": 1, "y": 1}, )
 
         page.wait_for_selector('text=pk 5 | tree 1 | lft 2 | rgt 3')
